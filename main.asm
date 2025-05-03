8de438dfc63019f3c7091493df9133527eb7c630
GalPovsod8 <galpos10@gmail.com>
2025-05-03 21:45:29 +0200

    NS-18 implemented: ustvarjanje zbirke datoteke

--- main.asm
+++ main.asm
@@ -1,6 +1,9 @@
 section .data
     dir_name db "28878_Dir", 0     ; Ime imenika (null-terminated string)
     dir_mode equ 0o755             ; Pravice za imenik (rwxr-xr-x)
+    
+    file_name db "28878_file.dat", 0     ; Ime datoteke
+    file_mode equ 0o644                  ; Privzeta dovoljenja za datoteko (rw-r--r--)
 
 section .text
     global _start
@@ -20,6 +23,16 @@ _start:
     mov eax, 12           ; syscall številka za chdir
     mov ebx, dir_name     ; EBX = kazalec na ime imenika
     int 0x80              ; izvedi sistemski klic (chdir)
+
+    ; ================================
+    ; KORAK 3: Ustvarjanje zbirke datoteke
+    ; ================================
+    mov eax, 5            ; syscall številka za open
+    mov ebx, file_name    ; EBX = ime datoteke
+    mov ecx, 0x41         ; ECX = zastavice: O_WRONLY | O_CREAT
+    mov edx, file_mode    ; EDX = dovoljenja: rw-r--r--
+    int 0x80              ; izvedi open
+    mov esi, eax          ; shrani datotečni opisovalec v ESI
 
     ; ================================
     ; KORAK X: Izhod iz programa
